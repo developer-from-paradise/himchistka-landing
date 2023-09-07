bad10f6ecf22b417d6e81a2b4b41f2d37a946e6c
insanekinge <insanekinge@icloud.com>
2023-09-07 14:09:39 +0300

    сделал кнопку в хедере по тому же принципу скопируй стили и сделай кнопку в других местах

--- .distr/blocks/form-fancybox/block.php
+++ .distr/blocks/form-fancybox/block.php
@@ -14,7 +14,7 @@
             </div>
             
             <button type="submit" class="form-fancybox__form-btn btn-b">
-                Заказать
+                <div class="btn-txt">Заказать</div>
                 <svg class="form-fancybox__form-btn-ico" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M6.86609 2.31819C7.23882 2.00901 7.79998 2.05078 8.11946 2.41149L15.2306 10.4402C15.5159 10.7623 15.5159 11.2377 15.2306 11.5598L8.11946 19.5885C7.79998 19.9492 7.23882 19.9909 6.86609 19.6818C6.49335 19.3726 6.45019 18.8295 6.76967 18.4688L13.2223 10.9999L6.76967 3.53113C6.45019 3.17042 6.49335 2.62737 6.86609 2.31819Z" fill="white"/>
                 </svg>
